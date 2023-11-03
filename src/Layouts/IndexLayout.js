6aa096ddc8d01a3d464de774921dde2f50746ef3
samuelrurangamirwa <rurangamirwa@gmail.com>
2023-11-03 12:53:41 +0200

    Remove errors

--- src/Layouts/IndexLayout.js
+++ src/Layouts/IndexLayout.js
@@ -5,25 +5,6 @@ import FooterBottom from "../components/home/Footer/FooterBottom";
 import { Outlet } from "react-router-dom";
 
 const IndexLayout = () => {
-  const Dispatch = useDispatch()
-  const Navigate = useNavigate()
-
-  useEffect(() => { 
-    const checkForGoogleUserInfo = async () => {
-      const response = await axios.get(`${process.env.REACT_APP_BACKEND_SERVER_URL}/auth/google/success`, { withCredentials: true })
-      .catch((error) => { 
-        console.log({ error: error.message });
-      })
-    
-      if (response && response.data) {
-        sessionStorage.setItem("token", response.data.token)
-        console.log("yes");
-        Dispatch(logIn({profile: response.data.user, logInType: "ByGoogle"}))
-        Navigate("/accounts/")
-      } 
-    }
-    checkForGoogleUserInfo()
-  },[])
 
   return (
     <div>
