31a63421936cbdab490fc4e60c234aeebe3a182e
samuelrurangamirwa <rurangamirwa@gmail.com>
2023-11-08 11:49:36 +0200

    Create ads

---
+++ src/components/imageslider/ImageSlider.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.css';
+import './ImageSlider.css';
+
+const ImageSlider = ({ ads }) => {
+  return (
+    <Swiper
+      slidesPerView={1}
+      spaceBetween={30}
+      loop={true}
+      className="image-slider h-full"
+    >
+      {ads.map((ad, index) => (
+        <SwiperSlide key={index}>
+          <div className="slider-item">
+            <img className='object-fit h-[15rem] rounded' src={ad.image} alt={ad.title} />
+            <div className="slider-title">{ad.title}</div>
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
+
+export default ImageSlider;
