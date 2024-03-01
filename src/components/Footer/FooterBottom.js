b1de73558de4d251d6354d766a4937a1c3ba1567
oliviertech212 <oliviertech212@gmail.com>
2024-03-01 15:35:48 +0200

    fix redirect product

--- src/components/Footer/FooterBottom.js
+++ src/components/Footer/FooterBottom.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import { AiOutlineCopyright } from 'react-icons/ai';
+import React from "react";
+import { AiOutlineCopyright } from "react-icons/ai";
 
 const FooterBottom = () => {
   return (
-    <div className='w-full bg-[#1D6F2B] group'>
-      <div className='max-w-container mx-auto border-t-[1px] py-8'>
-        <p className='text-titleFont font-normal text-center flex md:items-center justify-center text-white duration-200 text-sm'>
-          <span className='text-md mr-[1px] mt-[2px] md:mt-0 text-center hidden md:inline-flex'>
+    <div className="w-full bg-[#1D6F2B] group">
+      <div className="max-w-container mx-auto border-t-[1px] py-8">
+        <p className="text-titleFont font-normal text-center flex md:items-center justify-center text-white duration-200 text-sm">
+          <span className="text-md mr-[1px] mt-[2px] md:mt-0 text-center hidden md:inline-flex">
             <AiOutlineCopyright />
           </span>
           Copyright 2024 | Feli Global Market | All Rights Reserved |
-          <a href='https://reactbd.com/' target='_blank' rel='noreferrer'>
-            <span className='ml-1 font-medium group-hover:text-[#000]'>
+          <a
+            href="https://felitechnology.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <span className="ml-1 font-medium group-hover:text-[#000]">
               Powered by Felitechnology.com
             </span>
           </a>
