bcfcb12e2e2cb2e2e51e072fd774acc0e146615b
oliviertech212 <oliviertech212@gmail.com>
2024-03-19 07:08:27 +0200

    fix display for categories rebase from main

--- src/components/Header/SearchBar.js
+++ src/components/Header/SearchBar.js
@@ -1,28 +1,29 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { motion } from 'framer-motion';
-import MenuIcon from '../../assets/images/menu.png';
-import MenuIconWhite from '../../assets/images/menu-white.png';
-import { FaSearch } from 'react-icons/fa';
-import Flex from '../designLayouts/Flex';
-import { useNavigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { paginationItems } from '../../constants';
-import { useLocation } from 'react-router-dom';
+import React, { useState, useRef, useEffect } from "react";
+import { motion } from "framer-motion";
+import MenuIcon from "../../assets/images/menu.png";
+import MenuIconWhite from "../../assets/images/menu-white.png";
+import { FaSearch } from "react-icons/fa";
+import Flex from "../designLayouts/Flex";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { paginationItems } from "../../constants";
+import { useLocation } from "react-router-dom";
+import HomePageCategories from "../homePageCategories/HomePageCategories";
 
 const SearchBar = () => {
   const [showCategories, setShowCategories] = useState(false);
   const navigate = useNavigate();
   const location = useLocation();
   const ref = useRef();
-  useEffect(() => {
-    document.body.addEventListener('click', (e) => {
-      if (ref.current !== null && ref.current.contains(e.target) === false) {
-        return setShowCategories(false);
-      }
-    });
-  }, []);
+  // useEffect(() => {
+  //   document.body.addEventListener("click", (e) => {
+  //     if (ref.current !== null && ref.current.contains(e.target) === false) {
+  //       return setShowCategories(false);
+  //     }
+  //   });
+  // }, []);
 
-  const [searchQuery, setSearchQuery] = useState('');
+  const [searchQuery, setSearchQuery] = useState("");
   const [filteredProducts, setFilteredProducts] = useState([]);
   const [showSearchBar, setShowSearchBar] = useState(false);
 
@@ -53,10 +54,10 @@ const SearchBar = () => {
   }, [searchQuery]);
 
   return (
-    <div className='hidden md:block w-[30%] relative  '>
-      <div className='max-w-container mx-auto'>
-        <Flex className='flex lg:flex-row items-start lg:items-center justify-between lg:pb-0 h-full lg:h-24'>
-          <div
+    <div className="hidden md:block w-[30%] relative   ">
+      <div className="max-w-container mx-auto">
+        <Flex className="flex lg:flex-row items-start lg:items-center justify-between lg:pb-0 h-full lg:h-24">
+          {/* <div
             onClick={() => handleCategoriesMenuClick()}
             ref={ref}
             className={
@@ -103,16 +104,17 @@ const SearchBar = () => {
                 </li>
               </motion.ul>
             )}
-          </div>
-          <div className='relative w-full lg:w-[600px] h-[50px] text-base text-primeColor bg-white flex items-center gap-2 justify-between px-6 rounded-md border-[2px]'>
+          </div> */}
+
+          <div className="relative w-full lg:w-[600px] h-[50px] text-base text-primeColor bg-white flex items-center gap-2 justify-between px-6 rounded-md border-[2px]">
             <input
-              className='flex-1 h-full outline-none placeholder:text-[#C4C4C4] placeholder:text-[14px] border-none'
-              type='text'
+              className="flex-1 h-full outline-none placeholder:text-[#C4C4C4] placeholder:text-[14px] border-none"
+              type="text"
               onChange={handleSearch}
               value={searchQuery}
-              placeholder='Search your products here'
+              placeholder="Search your products here"
             />
-            <FaSearch className='w-5 h-5 ' />
+            <FaSearch className="w-5 h-5 " />
             {searchQuery && (
               <div
                 className={`w-full mx-auto h-96 bg-white top-16 absolute left-0 z-50 overflow-y-scroll shadow-2xl scrollbar-hide cursor-pointer`}
@@ -124,27 +126,27 @@ const SearchBar = () => {
                         navigate(`/products/${item.id}`);
 
                         setShowSearchBar(true);
-                        setSearchQuery('');
+                        setSearchQuery("");
                       }}
                       key={item.id}
-                      className='max-w-[600px] h-28 bg-gray-100 mb-3 flex items-center gap-3  break-words'
+                      className="max-w-[600px] h-28 bg-gray-100 mb-3 flex items-center gap-3  break-words"
                     >
                       <img
-                        className='w-24'
+                        className="w-24"
                         src={item?.productImages?.productThumbnail?.url}
-                        alt='productImg'
+                        alt="productImg"
                       />
-                      <div className='flex flex-col gap-1'>
-                        <p className='font-semibold text-lg'>{item.name}</p>
+                      <div className="flex flex-col gap-1">
+                        <p className="font-semibold text-lg">{item.name}</p>
                         <p
-                          className='text-xs overflow-auto break-words '
+                          className="text-xs overflow-auto break-words "
                           dangerouslySetInnerHTML={{
                             __html: item.description.slice(0, 80),
                           }}
                         ></p>
-                        <p className='text-sm'>
-                          Price:{' '}
-                          <span className='text-primeColor font-semibold'>
+                        <p className="text-sm">
+                          Price:{" "}
+                          <span className="text-primeColor font-semibold">
                             {item.currency} {item.price}
                           </span>
                         </p>
@@ -154,7 +156,7 @@ const SearchBar = () => {
               </div>
             )}
           </div>
-          <div className='flex gap-4 mt-2 lg:mt-0 items-center pr-6 cursor-pointer relative'></div>
+          <div className="flex gap-4 mt-2 lg:mt-0 items-center pr-6 cursor-pointer relative"></div>
         </Flex>
       </div>
     </div>
--- src/components/homePageCategories/HomePageCategories.js
+++ src/components/homePageCategories/HomePageCategories.js
@@ -1,10 +1,10 @@
-import { useState } from 'react';
-import axios from 'axios';
-import { HiChevronRight } from 'react-icons/hi2';
-import { Loader } from '../../dashboard/Components/Loader/LoadingSpin';
-import MenuIconWhite from '../../assets/images/menu-white.png';
-import { Link } from 'react-router-dom';
-import { useQuery } from '@tanstack/react-query';
+import { useState } from "react";
+import axios from "axios";
+import { HiChevronRight } from "react-icons/hi2";
+import { Loader } from "../../dashboard/Components/Loader/LoadingSpin";
+import MenuIconWhite from "../../assets/images/menu-white.png";
+import { Link } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
 
 export async function fetchCategories() {
   try {
@@ -14,7 +14,7 @@ export async function fetchCategories() {
 
     return response.data.data.categories;
   } catch (error) {
-    throw new Error('Error fetching categories');
+    throw new Error("Error fetching categories");
   }
 }
 
@@ -24,7 +24,7 @@ export default function HomePageCategories() {
     data: categories,
     error,
   } = useQuery({
-    queryKey: ['categories'],
+    queryKey: ["categories"],
     queryFn: fetchCategories,
   });
 
@@ -40,38 +40,38 @@ export default function HomePageCategories() {
 
   return (
     <div
-      className='hidden lg:flex flex-col gap-2 w-full bg-[#D9D9D970] h-full relative rounded'
+      className="hidden lg:flex flex-col gap-2 w-full bg-[#D9D9D970] h-full relative rounded"
       onMouseLeave={handleMouseLeave}
     >
-      <div className='flex items-center gap-3 p-2 rounded-t  bg-[#1D6F2B]'>
-        <img src={MenuIconWhite} className='w-5 h-5' />
-        <h3 className=' text-white text-lg'>Categories</h3>
+      <div className="flex items-center gap-3 p-2 rounded-t  bg-[#1D6F2B]">
+        <img src={MenuIconWhite} className="w-5 h-5" />
+        <h3 className=" text-white text-lg">Categories</h3>
       </div>
 
       {isLoading && (
-        <div className='flex justify-center p-16'>
+        <div className="flex justify-center p-16">
           <Loader />
         </div>
       )}
 
       {!isLoading && (
-        <div className='flex relative rounded h-full overflow-auto'>
-          <ul className='w-full flex flex-col rounded-b'>
+        <div className="flex relative rounded h-full overflow-auto">
+          <ul className="w-full flex flex-col rounded-b">
             {categories?.map((category) => (
               <li
                 className={`flex gap-4 cursor-pointer items-center justify-between hover:bg-[#1D6F2B] hover:text-white ${
                   hoveredCategory === category.name &&
-                  'bg-[#1D6F2B] text-white '
+                  "bg-[#1D6F2B] text-white "
                 }`}
                 key={category.name}
                 onMouseEnter={() => handleMouseEnter(category.id)}
               >
                 <Link
                   to={`shop/?category=${hoveredCategory}`}
-                  className='capitalize-first w-full p-2'
+                  className="capitalize-first w-full p-2"
                 >
                   {category.name}
-                </Link>{' '}
+                </Link>{" "}
                 {hoveredCategory === category.id && <HiChevronRight />}
               </li>
             ))}
@@ -80,8 +80,8 @@ export default function HomePageCategories() {
       )}
 
       {hoveredCategory && (
-        <div className=' bg-gray-200 text-gray-700 pt-6  b-0 absolute min-h-full rounded left-full z-50'>
-          <ul className='min-w-52 flex gap-2 flex-col'>
+        <div className=" bg-gray-200 text-gray-700 pt-6  b-0 absolute min-h-full rounded left-full z-50">
+          <ul className="min-w-52 flex gap-2 flex-col">
             {categories
               .find((cat) => {
                 return cat.id === hoveredCategory;
@@ -89,11 +89,11 @@ export default function HomePageCategories() {
               .subCategories.map((subCat) => (
                 <li
                   key={subCat.id}
-                  className='w-full px-4 hover:underline cursor-pointer'
+                  className="w-full px-4 hover:underline cursor-pointer"
                 >
                   <Link
                     to={`shop/?category=${hoveredCategory}&subcategory=${subCat.id}`}
-                    className='capitalize'
+                    className="capitalize"
                   >
                     {subCat.name}
                   </Link>
