551ca12cb0291cb94578720f54f85778ab491031
ericndungutse <dav.ndungutse@gmil.com>
2024-04-09 13:47:34 +0200

    filter products by color

--- src/components/pageProps/shopPage/shopBy/Brand.js
+++ src/components/pageProps/shopPage/shopBy/Brand.js
@@ -1,14 +1,14 @@
-import React, { useState } from "react";
-import { motion } from "framer-motion";
-import NavTitle from "./NavTitle";
-import { useSearchParams } from "react-router-dom";
+import React, { useState } from 'react';
+import { motion } from 'framer-motion';
+import NavTitle from './NavTitle';
+import { useSearchParams } from 'react-router-dom';
 
 const Brand = ({ brands, handlefilterShow }) => {
   const [showBrands, setShowBrands] = useState(true);
   const [searchParams, setSearchParams] = useSearchParams();
 
   const handleOnClickBrand = (brandName) => {
-    searchParams.set("brandName", brandName);
+    searchParams.set('brandName', brandName);
     setSearchParams(searchParams);
   };
 
@@ -16,9 +16,9 @@ const Brand = ({ brands, handlefilterShow }) => {
     <div>
       <div
         onClick={() => setShowBrands(!showBrands)}
-        className="cursor-pointer"
+        className='cursor-pointer'
       >
-        <NavTitle title="Shop by Brand" icons={true} />
+        <NavTitle title='Shop by Brand' icons={true} />
       </div>
       {showBrands && (
         <motion.div
@@ -26,15 +26,14 @@ const Brand = ({ brands, handlefilterShow }) => {
           animate={{ y: 0, opacity: 1 }}
           transition={{ duration: 0.5 }}
         >
-          <ul className="flex flex-col gap-4 text-sm lg:text-base text-[#767676]">
+          <ul className='flex flex-col gap-4 text-sm lg:text-base text-[#767676]'>
             {brands?.map((item) => (
               <li
                 key={item}
                 onClick={() => {
                   handleOnClickBrand(item);
-                  handlefilterShow();
                 }}
-                className="border-b-[1px] capitalize border-b-[#F0F0F0] pb-2 flex items-center gap-2 hover:text-primeColor hover:border-gray-400 duration-300 cursor-pointer"
+                className='border-b-[1px] capitalize border-b-[#F0F0F0] pb-2 flex items-center gap-2 hover:text-primeColor hover:border-gray-400 duration-300 cursor-pointer'
               >
                 {item}
               </li>
