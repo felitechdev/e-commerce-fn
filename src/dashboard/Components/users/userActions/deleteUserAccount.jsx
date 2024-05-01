35c92c4d35e270f5f084a993d7371ba2525744b6
oliviertech212 <oliviertech212@gmail.com>
2024-05-01 21:51:35 +0200

    implement admin delete user account

--- src/dashboard/Components/users/userActions/deleteUserAccount.jsx
+++ src/dashboard/Components/users/userActions/deleteUserAccount.jsx
@@ -4,25 +4,42 @@ import { ExclamationCircleFilled } from "@ant-design/icons";
 import { useUser } from "../../../../context/UserContex";
 import axios from "axios";
 import { DeleteFilled } from "@ant-design/icons";
+import Cookies from "js-cookie";
 
 const { confirm } = Modal;
 
-const DeleteConfirmation = ({ id, token }) => {
-  const { token: userToken } = useUser();
+const DeleteUserAccountConfirmation = ({
+  openDeleteModal,
+  setOpenDeleteModal,
+  id,
+}) => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
   const [err, setErr] = useState("");
   const [onSuccess, setOnSuccess] = useState(null);
+  const token = Cookies.get("token");
+  console.log(
+    "id",
+    id,
+    "token",
+    token,
+    "userToken",
+
+    "openDeleteModal",
+    openDeleteModal,
+    "setOpenDeleteModal",
+    setOpenDeleteModal
+  );
 
   const handleDelete = async () => {
     try {
       setLoading(true);
       const response = await axios({
-        url: `${process.env.REACT_APP_BACKEND_SERVER_URL}/api/v1/auth/delete-account/${id}`,
+        url: `${process.env.REACT_APP_BACKEND_SERVER_URL}/api/v1/auth/delete-account/${id.id}`,
         method: "DELETE",
         headers: {
           "content-type": "application/json",
-          Authorization: userToken ? `Bearer ${userToken}` : `Bearer ${token}`,
+          Authorization: token && `Bearer ${token}`,
         },
       });
 
@@ -63,19 +80,17 @@ const DeleteConfirmation = ({ id, token }) => {
       okType: "danger",
       cancelText: "No",
       onOk: handleDelete,
-      onCancel() {},
+      onCancel() {
+        setOpenDeleteModal(false);
+      },
     });
   };
 
   return (
-    <div
-      className=" cursor-pointer md:flex items-center md:border p-1  md:space-x-2 font-bold text-[red] "
-      onClick={showDeleteConfirm}
-    >
-      <DeleteFilled className=" text-icon3" />
-      <h1 className="text-sm">Delete Account</h1>
+    <div className=" cursor-pointer md:flex items-center md:border p-1  md:space-x-2 font-bold text-[red] ">
+      {openDeleteModal && showDeleteConfirm}
     </div>
   );
 };
 
-export default DeleteConfirmation;
+export default DeleteUserAccountConfirmation;
