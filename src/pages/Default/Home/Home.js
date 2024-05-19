72b243d5504967e6d44b88964f5a3e4307edb3d2
oliviertech212 <oliviertech212@gmail.com>
2024-05-19 16:03:38 +0200

    handle get product class on home page

--- src/pages/Default/Home/Home.js
+++ src/pages/Default/Home/Home.js
@@ -9,43 +9,50 @@ const Home = () => {
   const [selectedCategory, setSelectedCategory] = useState({
     category: {
       categoryname: null,
-      categoryId: null
+      categoryId: null,
     },
     subcategory: {
       subcategoryname: null,
-      subcategoryId: null
-    }
+      subcategoryId: null,
+    },
   });
 
   const handleCategorySelect = (category, subcategory) => {
     if (category.categoryId) {
       setSelectedCategory({
-        category: { categoryname: category.categoryname, categoryId: category.categoryId },
-        subcategory: { subcategoryname: null, subcategoryId: null }
-      })
+        category: {
+          categoryname: category.categoryname,
+          categoryId: category.categoryId,
+        },
+        subcategory: { subcategoryname: null, subcategoryId: null },
+      });
     } else if (subcategory.subcategoryId) {
       setSelectedCategory({
         category: { categoryname: null, categoryId: null },
-        subcategory: { subcategoryname: subcategory.subcategoryname, subcategoryId: subcategory.subcategoryId }
+        subcategory: {
+          subcategoryname: subcategory.subcategoryname,
+          subcategoryId: subcategory.subcategoryId,
+        },
       });
-    } 
+    }
   };
   return (
-    <div className="w-full mx-auto">
-      <Banner onCategorySelect={handleCategorySelect}/>
-      {selectedCategory.category.categoryId || selectedCategory.subcategory.subcategoryId ? (
+    <div className="w-full mx-auto bg-black">
+      <Banner onCategorySelect={handleCategorySelect} />
+      {selectedCategory.category.categoryId ||
+      selectedCategory.subcategory.subcategoryId ? (
         <div className="max-w-container mx-auto px-4">
           <CategoryFilteredProducts selectedCategory={selectedCategory} />
-        </div>  
-        ) : (
-            <>
-              <NewArrivals />
-              <div className="max-w-container mx-auto px-4">
-                <AllProducts />
-                <YearProduct />
-              </div>
-            </>
-          )}
+        </div>
+      ) : (
+        <>
+          <NewArrivals />
+          <div className="max-w-container mx-auto px-4">
+            <AllProducts />
+            <YearProduct />
+          </div>
+        </>
+      )}
     </div>
   );
 };
