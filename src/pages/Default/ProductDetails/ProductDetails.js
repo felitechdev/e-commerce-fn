05eb5e676fc476a45a4f2edbf4dcdf1b76fbf6ac
oliviertech212 <oliviertech212@gmail.com>
2024-06-27 19:25:08 +0200

    change product image to opject cover

--- src/pages/Default/ProductDetails/ProductDetails.js
+++ src/pages/Default/ProductDetails/ProductDetails.js
@@ -1,14 +1,14 @@
-import React, { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
-import ProductMainInfo from '../../../components/pageProps/productDetails/ProductMainInfo';
-import ProductImages from '../../../components/pageProps/productDetails/ProductImages';
-import CheckoutDetails from '../../../components/pageProps/productDetails/CheckoutDetails';
-import ProductSecondaryInfo from '../../../components/pageProps/productDetails/ProductSecondaryInfo';
-import ProductsSection from '../../../components/home/Products/ProductsSection';
-import Product from '../../../components/home/Products/Product';
-import ProductsSliderContainer from '../../../components/home/Products/ProductsSliderContainer';
-import { useSelector } from 'react-redux';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import ProductMainInfo from "../../../components/pageProps/productDetails/ProductMainInfo";
+import ProductImages from "../../../components/pageProps/productDetails/ProductImages";
+import CheckoutDetails from "../../../components/pageProps/productDetails/CheckoutDetails";
+import ProductSecondaryInfo from "../../../components/pageProps/productDetails/ProductSecondaryInfo";
+import ProductsSection from "../../../components/home/Products/ProductsSection";
+import Product from "../../../components/home/Products/Product";
+import ProductsSliderContainer from "../../../components/home/Products/ProductsSliderContainer";
+import { useSelector } from "react-redux";
+import axios from "axios";
 // change i made
 const ProductDetails = () => {
   const location = useLocation();
@@ -16,16 +16,14 @@ const ProductDetails = () => {
   const [cartItemInfo, setCartItemInfo] = useState({
     productDBId: location.state.productId,
     quantity: 0,
-    colorId: '',
-    size: '',
+    colorId: "",
+    size: "",
   });
 
   const [apiData, setApiData] = useState([]);
   const [duplicatedData, setDuplicatedData] = useState([]);
 
-  const userInfo = useSelector(
-    (state) => state.userReducer.userInfo
-  );
+  const userInfo = useSelector((state) => state.userReducer.userInfo);
 
   useEffect(() => {
     const fetchProductInfo = async () => {
@@ -37,13 +35,11 @@ const ProductDetails = () => {
 
         setCartItemInfo({
           ...cartItemInfo,
-          imagePreview:
-            productInfo.data.productImages.productThumbnail
-              .url,
+          imagePreview: productInfo.data.productImages.productThumbnail.url,
         });
       } catch (error) {
         console.error(
-          'Error fetching data product on product details :',
+          "Error fetching data product on product details :",
           error
         );
       }
@@ -61,7 +57,7 @@ const ProductDetails = () => {
         }
       } catch (error) {
         console.error(
-          'Error fetching data product on product details :',
+          "Error fetching data product on product details :",
           error
         );
       }
@@ -72,11 +68,11 @@ const ProductDetails = () => {
   }, [location.state.productId]);
 
   return (
-    <div className='w-full mx-auto border-b-[1px] border-b-gray-300'>
-      <div className='max-w-container mx-auto p-4 mt-10'>
-        <div className='w-full  h-full -mt-5 xl:-mt-8 pb-10'>
-          <div className='flex flex-col gap-14'>
-            <div className='flex flex-col mdl:flex-row mdl:flex-wrap gap-12'>
+    <div className="w-full mx-auto border-b-[1px] border-b-gray-300">
+      <div className="max-w-container mx-auto p-4 mt-10">
+        <div className="w-full  h-full -mt-5 xl:-mt-8 pb-10">
+          <div className="flex flex-col gap-14">
+            <div className="flex flex-col mdl:flex-row mdl:flex-wrap gap-12">
               {Object.keys(DBProductInfo).length > 0 ? (
                 <>
                   <ProductImages
@@ -96,28 +92,21 @@ const ProductDetails = () => {
                     cartItemInfo={cartItemInfo}
                     setCartItemInfo={setCartItemInfo}
                   />
-                  <ProductSecondaryInfo
-                    DBProductInfo={DBProductInfo}
-                  />
+                  <ProductSecondaryInfo DBProductInfo={DBProductInfo} />
                 </>
               ) : (
-                ''
+                ""
               )}
             </div>
           </div>
           {/* For testing similar products slider only */}
-          <ProductsSection heading='Similar Products'>
+          <ProductsSection heading="Similar Products">
             <ProductsSliderContainer>
-              {duplicatedData.map(
-                (product, productIndex) => (
-                  <div
-                    key={product._id + productIndex}
-                    className='px-2'
-                  >
-                    <Product productInfo={product} />
-                  </div>
-                )
-              )}
+              {duplicatedData.map((product, productIndex) => (
+                <div key={product._id + productIndex} className="px-2">
+                  <Product productInfo={product} />
+                </div>
+              ))}
             </ProductsSliderContainer>
           </ProductsSection>
         </div>
