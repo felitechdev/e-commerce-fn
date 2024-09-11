ae9be6cc8ec25b60dd0118f219d482e9c75275c0
oliviertech212 <oliviertech212@gmail.com>
2024-09-11 11:31:29 +0200

    change featured api

--- src/APIs/react-query/featured-product.js
+++ src/APIs/react-query/featured-product.js
@@ -6,7 +6,7 @@ import axios from "axios";
 export async function featuredproducts() {
   try {
     const response = await axios.get(
-      `${process.env.REACT_APP_BACKEND_SERVER_URL}/api/v1/products/?featured.isFeatured=true`
+      `${process.env.REACT_APP_BACKEND_SERVER_URL}/api/v1/products?featured[isFeatured]=true`
     );
 
     return response.data;
