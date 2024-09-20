5bc443ca39ff058e19d0ea804824c59edaaf62f1
oliviertech212 <oliviertech212@gmail.com>
2024-09-20 08:56:59 +0200

    fix length

--- src/components/ProductsCategories.js
+++ src/components/ProductsCategories.js
@@ -141,11 +141,13 @@ function ProductsCategories() {
       )}
 
       <div className="  max-w-container mx-auto px-2 md:px-6 space-y-4 my-10">
-        {products.length > 0 && <h1 className="medium_text">New Arrivals</h1>}
+        {products && products?.length > 0 && (
+          <h1 className="medium_text">New Arrivals</h1>
+        )}
         <div
           className={`relative bg-[#f8f8f8] rounded-md p-4 h-72 
 
-        ${products.length > 0 ? "" : "hidden"}
+        ${products && products?.length > 0 ? "" : "hidden"}
           
           
           `}
