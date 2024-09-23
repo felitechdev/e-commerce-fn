93f665abfdfef4f1bd6f86f1e6ffa530787a94e1
oliviertech212 <oliviertech212@gmail.com>
2024-09-23 12:16:53 +0200

    fix scroll to the top

--- src/pages/ProductDetails/ProductDetails.js
+++ src/pages/ProductDetails/ProductDetails.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import ProductImages from "./ProductImages";
 import ProductMainInfo from "./ProductMainInfo";
 import CheckoutDetails from "./CheckoutDetails";
@@ -11,17 +11,25 @@ import { ProductSection } from "../../components/our-products/ProductSection";
 export default function ProductDetails({ product, dispatch }) {
   const [sectionHasProducts, setSectionHasProducts] = useState({});
 
+  const topDivRef = useRef(null);
+  const scrollToTop = () => {
+    if (topDivRef.current) {
+      setTimeout(() => {
+        topDivRef.current.scrollIntoView({ behavior: 'smooth' });
+      }, 1000);
+     
+    }
+  };
   const handleAddwishlist = async (id) => {
     // event.stopPropagation();
 
     let productInfo = product.productDetails;
-
+   
     let wishlist = JSON.parse(localStorage.getItem("wishlist"));
 
     if (!wishlist) {
       wishlist = [];
     }
-
     let existingProduct = wishlist.find((product) => product.id === id);
 
     if (!existingProduct) {
@@ -64,8 +72,8 @@ export default function ProductDetails({ product, dispatch }) {
     <div className="w-full mx-auto border-b-[1px] border-b-gray-300">
       <div className="max-w-container mx-auto p-4 mt-10">
         <div className="w-full  h-full -mt-5 xl:-mt-8 pb-10">
-          <div className="flex flex-col gap-14">
-            <div className="flex flex-col mdl:flex-row mdl:flex-wrap gap-12 items-center">
+          <div className="flex flex-col gap-14 ">
+            <div  ref={topDivRef} className="flex flex-col  mdl:flex-row mdl:flex-wrap gap-12 items-center">
               <>
                 <ProductImages
                   productImages={product.productDetails.productImages}
@@ -88,7 +96,7 @@ export default function ProductDetails({ product, dispatch }) {
             </div>
           </div>
 
-          <div className="max-w-container mx-auto px-2 md:px-6 space-y-4 mt-10 ">
+          <div onClick={scrollToTop}  className="max-w-container  mx-auto px-2 md:px-6 space-y-4 mt-10 ">
             <h1 className="medium2_text my-6 ">Related Products</h1>
             <ProductSection
               // key={`${productClass.id}`}
