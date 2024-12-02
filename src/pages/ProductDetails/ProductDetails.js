fc95638c14ecb71829a562d31dccd8e55a981b4a
oliviertech212 <oliviertech212@gmail.com>
2024-12-02 11:13:58 +0200

    change single product page view

--- src/pages/ProductDetails/ProductDetails.js
+++ src/pages/ProductDetails/ProductDetails.js
@@ -1,4 +1,4 @@
-import React, { useState,useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import ProductImages from "./ProductImages";
 import ProductMainInfo from "./ProductMainInfo";
 import CheckoutDetails from "./CheckoutDetails";
@@ -7,34 +7,33 @@ import { useDispatch } from "react-redux";
 import { addTowishlist } from "../../redux/Reducers/wishlist";
 import { removeTowishlist } from "../../redux/Reducers/wishlist";
 import discountedFinalPrice from "../../util/discountedFinalPrice";
-import { ProductSection } from "../../components/our-products/ProductSection";
+import {
+  ProductSection,
+  RelatedProducts,
+} from "../../components/our-products/ProductSection";
 export default function ProductDetails({ product, dispatch }) {
   const [sectionHasProducts, setSectionHasProducts] = useState({});
 
   const topDivRef = useRef(null);
- 
 
   const [visible, setVisible] = useState(false);
 
-
   const toggleVisible = () => {
-      const scrolled = document.documentElement.scrollTop;
-      if (scrolled > 300) {
-          setVisible(true);
-      } else if (scrolled <= 300) {
-          setVisible(false);
-      }
+    const scrolled = document.documentElement.scrollTop;
+    if (scrolled > 300) {
+      setVisible(true);
+    } else if (scrolled <= 300) {
+      setVisible(false);
+    }
   };
 
   const scrollToTop = () => {
     setTimeout(() => {
       window.scrollTo({
         top: 0,
-        behavior: "smooth"
-     
-    }); 
+        behavior: "smooth",
+      });
     }, 2000);
-      
   };
 
   window.addEventListener("scroll", toggleVisible);
@@ -42,7 +41,7 @@ export default function ProductDetails({ product, dispatch }) {
     // event.stopPropagation();
 
     let productInfo = product.productDetails;
-   
+
     let wishlist = JSON.parse(localStorage.getItem("wishlist"));
 
     if (!wishlist) {
@@ -57,8 +56,8 @@ export default function ProductDetails({ product, dispatch }) {
         price: Math.trunc(
           await discountedFinalPrice(
             productInfo.price,
-            productInfo.discountPercentage
-          )
+            productInfo.discountPercentage,
+          ),
         ),
         productThumbnail: productInfo.productImages.productThumbnail,
         seller: productInfo.seller,
@@ -86,65 +85,90 @@ export default function ProductDetails({ product, dispatch }) {
     }));
   };
 
+  // const productThumbnail =
+  // productInfo.productImages.productThumbnail.url.replace(
+  //   productInfo.productImages.productThumbnail.url.split("/")[6],
+  //   "w_300,h_300",
+  // );
+
   return (
-    <div className="w-full mx-auto border-b-[1px] border-b-gray-300">
-      <div className="max-w-container mx-auto p-4 mt-10">
-        <div className="w-full  h-full -mt-5 xl:-mt-8 pb-10">
-          <div className="flex flex-col gap-1 ">
-            <div  ref={topDivRef} className="h-1 cursor-pointer"></div>
-            <div   className="flex flex-col   sml:flex-row sml:flex-wrap gap-12 items-start ">
-              
-                <ProductImages
-               
-                  productImages={product.productDetails.productImages}
-                  activeImage={product.activeImage}
-                  dispatch={dispatch}
-                  productId={product.productDetails.id}
-                  handleAddwishlist={handleAddwishlist}
-                />
-                 <ProductMainInfo
-                  product={product.productDetails}
-                  dispatch={dispatch}
-                  selectedMeasurement={product.selectedMeasurement}
-                  selectedColor={product.selectedColor}
-                  activeImage={product.activeImage}
-                />
-               
-                {/* <CheckoutDetails product={product} /> */}
-
-                {/* <ProductSecondaryInfo product={product.productDetails} /> */}
-              
+    <div className="mx-auto w-full border-b-[1px] border-b-gray-300">
+      <div className="mx-auto mt-10 max-w-container p-4">
+        <div className="-mt-5 h-full w-full pb-10 xl:-mt-8">
+          <div className="flex flex-col gap-1">
+            <div ref={topDivRef} className="h-1 cursor-pointer"></div>
+            <div className="flex flex-col items-start justify-between gap-12 bg-gray-100 p-2 sml:flex-row sml:flex-wrap">
+              <ProductImages
+                productImages={product.productDetails.productImages}
+                activeImage={product.activeImage}
+                dispatch={dispatch}
+                productId={product.productDetails.id}
+                handleAddwishlist={handleAddwishlist}
+              />
+              <ProductMainInfo
+                product={product.productDetails}
+                dispatch={dispatch}
+                selectedMeasurement={product.selectedMeasurement}
+                selectedColor={product.selectedColor}
+                activeImage={product.activeImage}
+              />
+              {/* <CheckoutDetails product={product} /> */}
+              {/* <ProductSecondaryInfo product={product.productDetails} /> */}
             </div>
-          </div>
+            {product.productDetails?.attributes?.length > 0 &&
+              product.productDetails?.attributes[0].key !== null && (
+                <div className="mt-4 grid gap-5 md:grid-cols-2">
+                  <table className="h-fit table-auto rounded shadow-lg">
+                    <p className="py-2 text-xl font-semibold capitalize">
+                      Specifications :
+                    </p>
+                    {product.productDetails?.attributes.map(
+                      (attribute, index) => {
+                        return (
+                          <tr className="border">
+                            <td className="border border-black">
+                              {attribute.key}
+                            </td>
+                            <td className="border border-black">
+                              {attribute.value}
+                            </td>
+                          </tr>
+                        );
+                      },
+                    )}
+                  </table>
 
-          <div onClick={scrollToTop}  className="max-w-container  mx-auto px-2 md:px-6 space-y-4 mt-10 ">
-            <h1 className="medium2_text my-6 ">Related Products</h1>
-            <ProductSection
-              // key={`${productClass.id}`}
-              productClassId={product?.productDetails?.productClass}
-              // category={product?.productDetails?.category}
-              // setIsSectionHasProduct={(hasProducts) =>
-              //   handleSectionProductStatus(
-              //     product?.productDetails?.productClass,
-              //     hasProducts
-              //   )
-              // }
-            />
-          </div>
+                  <div className=" " onClick={scrollToTop}>
+                    <p className="py-2 text-xl font-semibold capitalize">
+                      Related Products
+                    </p>
 
-          {/* For testing similar products slider only */}
-          {/* <ProductsSection heading='Similar Products'>
-            <ProductsSliderContainer>
-              {similarProducts.length &&
-                similarProducts.map((product) => {
-                  return (
-                    <div key={product._id} className='px-2'>
-                      <ProductPreview productInfo={product} />
-                    </div>
-                  );
-                })}
-            </ProductsSliderContainer>
-          </ProductsSection> */}
+                    <RelatedProducts
+                      productClassId={product?.productDetails?.productClass}
+                    />
+                  </div>
+                </div>
+              )}
+          </div>
+          {product.productDetails?.attributes[0]?.key == null && (
+            <div
+              onClick={scrollToTop}
+              className="mx-auto mt-10 max-w-container space-y-4"
+            >
+              <h1 className="medium2_text my-6">Related Products</h1>
+              <ProductSection
+                // key={`${productClass.id}`}
+                productClassId={product?.productDetails?.productClass}
+                // category={product?.productDetails?.category}
+                // setIsSectionHasProduct={(hasProducts) =>
+                //   handleSectionProductStatus(
+                //     product?.productDetails?.productClass,
+                //     hasProducts
+                //   )
+                // }
+              />
+            </div>
+          )}
         </div>
       </div>
     </div>
