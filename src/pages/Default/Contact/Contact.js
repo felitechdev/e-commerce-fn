74934474edd442e28dd0b13f01ca20abba3983fa
oliviertech212 <oliviertech212@gmail.com>
2025-01-04 16:14:31 +0200

    create formfield object configuration for react hook form handle errors display on ui show loading state and display message to the users handle form submition state and reset contact form on success

--- src/pages/Default/Contact/Contact.js
+++ src/pages/Default/Contact/Contact.js
@@ -1,15 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
-import Breadcrumbs from "../../../components/pageProps/Breadcrumbs";
+import { useForm } from "react-hook-form";
+import axios from "axios";
 import PageLayout from "../../../components/designLayouts/PageLayout";
 import { MdOutlineEmail } from "react-icons/md";
 import { SlLocationPin } from "react-icons/sl";
 import { FaInstagramSquare } from "react-icons/fa";
 import { FaFacebookSquare } from "react-icons/fa";
-import { FiPhoneCall } from "react-icons/fi";
+import { FiPhoneCall } from "react-icons/fi"; 
 import { FaLinkedin } from "react-icons/fa6";
 
 import { TbWorld } from "react-icons/tb";
+import AlertComponent from "../../../components/designLayouts/AlertComponent";
 
 const Contact = () => {
   const location = useLocation();
@@ -18,63 +20,121 @@ const Contact = () => {
     // setPrevLocation(location.state.data);
   }, [location]);
 
-  const [clientName, setclientName] = useState("");
-  const [email, setEmail] = useState("");
-  const [messages, setMessages] = useState("");
+const [loading, setLoading] = useState(false);
 
-  // ========== Error Messages Start here ============
-  const [errClientName, setErrClientName] = useState("");
-  const [errEmail, setErrEmail] = useState("");
-  const [errMessages, setErrMessages] = useState("");
-  // ========== Error Messages End here ==============
-  const [successMsg, setSuccessMsg] = useState("");
 
-  const handleName = (e) => {
-    setclientName(e.target.value);
-    setErrClientName("");
-  };
-  const handleEmail = (e) => {
-    setEmail(e.target.value);
-    setErrEmail("");
-  };
-  const handleMessages = (e) => {
-    setMessages(e.target.value);
-    setErrMessages("");
-  };
+const [message, setMessage] = useState(null);
+const [error, setError] = useState(null);
 
-  // ================= Email Validation start here =============
-  const EmailValidation = (email) => {
-    return String(email)
-      .toLowerCase()
-      .match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i);
-  };
-  // ================= Email Validation End here ===============
 
-  const handlePost = (e) => {
-    e.preventDefault();
-    if (!clientName) {
-      setErrClientName("Enter your Name");
-    }
-    if (!email) {
-      setErrEmail("Enter your Email");
-    } else {
-      if (!EmailValidation(email)) {
-        setErrEmail("Enter a Valid Email");
+
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
+  const onSubmit = async (data) => {
+   
+    setLoading(true);
+    try {
+      const result = await axios({
+        url: `${process.env.REACT_APP_BACKEND_SERVER_URL}/api/v1/contact-us`,
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        data: {
+          firstName: data.firstName,  
+          lastName: data.lastName,
+          email: data.email,
+          phoneNumber: data.phone.slice(1),
+          message: data.message,
+        }
+      });
+
+  
+
+      if (result.status === 200) {
+        setMessage( result?.data?.message || "Your message has been sent successfully");
+        setLoading(false);
+
+       document.getElementById("contact-form").reset();
       }
+
+
+
+
+
+      
+    } catch (error) {
+   
+      setError(error?.response?.data.message);
+      setLoading(false);
+      
+    } finally {
+      setLoading(false);
+
+      setTimeout(() => {
+        setError(null);
+        setMessage(null);
+      }
+      , 10000);
     }
-    if (!messages) {
-      setErrMessages("Enter your Messages");
-    }
-    if (clientName && email && EmailValidation(email) && messages) {
-      setSuccessMsg(
-        `Thank you dear ${clientName}, Your messages has been received successfully. Futher details will sent to you by your email at ${email}.`,
-      );
-    }
+
   };
+
+  const formFields = {
+    firstName: {
+      type: "text",
+      placeholder: "First name",
+      validation: {
+        required: "First name is required",
+        minLength: { value: 2, message: "First name must be at least 2 characters long" },
+      },
+    },
+    lastName: {
+      type: "text",
+      placeholder: "Last name",
+      validation: {
+        required: "Last name is required",
+        minLength: { value: 2, message: "Last name must be at least 2 characters long" },
+      },
+    },
+    email: {
+      type: "email",
+      placeholder: "you@example.com",
+      validation: {
+        required: "Email is required",
+        pattern: {
+          value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+          message: "Invalid email format",
+        },
+      },
+    },
+    phone: {
+      type: "text",
+      placeholder: "+250 7xxxxxxx",
+      validation: {
+        required: "Phone number is required",
+        pattern: {
+          value: /^\+2507[0-9]{8}$/,
+          message:
+            "Phone number must start with +250 and have 12 digits (e.g., +2507XXXXXXXX)",
+        },
+       
+      },
+    },
+    message: {
+      type: "textarea",
+      placeholder: "Leave us a message...",
+      validation: {
+        required: "Message is required",
+        minLength: { value: 10, message: "Message must be at least 10 characters long" },
+      },
+    },
+  };
+
 
   return (
     <PageLayout showFooter={true}>
-      <div className="mx-auto max-w-container px-6">
+      <div className="mx-auto max-w-container px-6 mb-5">
         {/* Header Section */}
         <div className="flex flex-col gap-8 rounded !px-28 !py-16 !text-center tracking-wider">
           <h1 className="font-titleFont !text-2xl font-extralight uppercase tracking-widest md:!text-3xl">
@@ -157,41 +217,53 @@ const Contact = () => {
           {/* Form Section */}
           <div className="rounded-lg border bg-gray-50 p-6">
             <h2 className="mb-6 text-3xl font-semibold">Send us a Message</h2>
-            <form className="space-y-6">
-              {/* Name */}
-              <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
-                <div>
-                  <label
-                    className="mb-1 block text-sm font-medium"
-                    htmlFor="firstName"
-                  >
-                    First name
-                  </label>
-                  <input
-                    id="firstName"
-                    type="text"
-                    placeholder="First name"
-                    className="w-full rounded border px-3 py-2"
-                  />
-                </div>
-                <div>
-                  <label
-                    className="mb-1 block text-sm font-medium"
-                    htmlFor="lastName"
-                  >
-                    Last name
-                  </label>
-                  <input
-                    id="lastName"
-                    type="text"
-                    placeholder="Last name"
-                    className="w-full rounded border px-3 py-2"
-                  />
-                </div>
-              </div>
 
-              {/* Email */}
-              <div>
+            {error && message == null && (
+              <AlertComponent
+                color="failure"
+                type="Error!"
+                message={error}
+              />
+            )}
+
+            {message != null && (
+              <AlertComponent
+                color="success"
+                type="Success!"
+                message={message}
+              />
+            )}
+
+
+
+
+            <form id="contact-form" className="space-y-6 mt-2 " onSubmit={handleSubmit(onSubmit)}>
+        {/* First Name and Last Name in Grid Layout */}
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+          {["firstName", "lastName"].map((field) => (
+            <div key={field}>
+              <label
+                className="mb-1 block text-sm font-medium"
+                htmlFor={formFields[field].id}
+              >
+                {formFields[field].label}
+              </label>
+              <input
+                {...register(field, formFields[field].validation)}
+                id={formFields[field].id}
+                type="text"
+                placeholder={formFields[field].placeholder}
+                className="w-full rounded border px-3 py-2"
+              />
+              {errors[field] && (
+                <p className="mt-1 text-sm text-red-600">
+                  {errors[field].message}
+                </p>
+              )}
+            </div>
+          ))}
+        </div>
+        <div>
                 <label
                   className="mb-1 block text-sm font-medium"
                   htmlFor="email"
@@ -199,58 +271,74 @@ const Contact = () => {
                   Email
                 </label>
                 <input
-                  id="email"
+                  id={formFields.email.id}
                   type="email"
+                  {...register("email", formFields.email.validation)}
                   placeholder="you@gmail.com"
                   className="w-full rounded border px-3 py-2"
                 />
+                 {errors.email && (
+            <p className="mt-1 text-sm text-red-600">
+              {errors.email.message}
+            </p>
+          )}
               </div>
 
-              {/* Phone */}
-              <div>
-                <label
-                  className="mb-1 block text-sm font-medium"
-                  htmlFor="phone"
-                >
-                  Phone number
-                </label>
-                <input
-                  id="phone"
-                  type="text"
-                  placeholder="+250 7xxxxxxx"
-                  className="w-full rounded border px-3 py-2"
-                />
-              </div>
+        {/* Phone Number */}
+        <div>
+          <label
+            className="mb-1 block text-sm font-medium"
+            htmlFor={formFields.phone.id}
+          >
+            {formFields.phone.label}
+          </label>
+          <input
+            {...register("phone", formFields.phone.validation)}
+            id={formFields.phone.id}
+            type="text"
+            placeholder={formFields.phone.placeholder}
+            className="w-full rounded border px-3 py-2"
+          />
+          {errors.phone && (
+            <p className="mt-1 text-sm text-red-600">
+              {errors.phone.message}
+            </p>
+          )}
+        </div>
 
-              {/* Message */}
-              <div>
-                <label
-                  className="mb-1 block text-sm font-medium"
-                  htmlFor="message"
-                >
-                  Message
-                </label>
-                <textarea
-                  id="message"
-                  rows="4"
-                  placeholder="Leave us a message……"
-                  className="w-full resize-none rounded border px-3 py-2"
-                ></textarea>
-              </div>
+        {/* Message */}
+        <div>
+          <label
+            className="mb-1 block text-sm font-medium"
+            htmlFor={formFields.message.id}
+          >
+            {formFields.message.label}
+          </label>
+          <textarea
+            {...register("message", formFields.message.validation)}
+            id={formFields.message.id}
+            placeholder={formFields.message.placeholder}
+            className="w-full resize-none rounded border px-3 py-2"
+            rows="4"
+          ></textarea>
+          {errors.message && (
+            <p className="mt-1 text-sm text-red-600">
+              {errors.message.message}
+            </p>
+          )}
+        </div>
 
-              {/* Submit Button */}
-              <button
-                type="submit"
-                onClick={
-                  (e) => handlePost(e)
-                  // console.log("Submit Button Clicked")
-                }
-                className="w-full rounded bg-primary py-2 text-white hover:bg-green-800"
-              >
-                Submit
-              </button>
+        {/* Submit Button */}
+        <button
+          type="submit"
+          className="w-full rounded bg-primary py-2 text-white hover:bg-green-800"
+          disabled={loading}
 
-              <p className="mt-4 text-sm text-gray-500">
+        >
+          {loading ? "Sending..." : "Send Message"}
+        </button>
+
+        <p className="mt-4 text-sm text-gray-500">
                 By contacting us, you agree to our{" "}
                 <Link
                   to="/terms-and-conditions"
@@ -268,7 +356,8 @@ const Contact = () => {
                 </Link>
                 .
               </p>
-            </form>
+      </form>
+       
           </div>
         </div>
       </div>
